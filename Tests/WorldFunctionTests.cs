5520ab329d0c20d7e59aef55a6ccb24ad843197b
RahulWhiztekcorp <rahul@whiztekcorp@gmail.com>
2023-11-22 15:18:20 +0530

    Add project files.

---
+++ Tests/WorldFunctionTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestConsoleApp
+{
+    public class WorldFunctionTests
+    {
+        // Naming Convenctions = ClassName_MethodName_ExepecctedResult
+
+        public static void WorldFunction_ReturnRahulIfZero_ReturnString()
+        {
+            try
+            {
+                // Arrange - Go get your variable , whatever you need ,you classes ,go get fuctions
+                int num = 1;
+                WorldFunction worldFunction = new WorldFunction();
+
+                // Art - Execute this function
+                string result = worldFunction.ReturnRahulIfZero(num);
+
+                // Assert - Whatever ever is returned is it what you want.
+                if (result == "Rahul")
+                {
+                    Console.WriteLine("PASSED: WorldFunction_ReturnRahulIfZero_ReturnString");
+                }
+                else
+                {
+                    Console.WriteLine("FAILED: WorldFunction_ReturnRahulIfZero_ReturnString");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+    }
+}
---
+++ WorldFunction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestConsoleApp
+{
+    public class WorldFunction
+    {
+        public string ReturnRahulIfZero(int num)
+        {
+            if (num == 0)
+            {
+                return "Rahul";
+            }
+            else
+            {
+                return "Send Zero Number";
+            }
+        }
+    }
+}
